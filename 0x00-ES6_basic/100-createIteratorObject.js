5ab0f93d7d54d5448ff53777b6867035d01a7363
oluwasube <enibeke20@gmail.com>
2022-12-23 18:48:19 +0000

    adavance task 1 completed

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,22 +1,7 @@
 export default function createIteratorObject(report) {
-  const departments = Object.keys(report.allEmployees);
-  let currentDepartment = 0;
-  let currentEmployee = 0;
-
-  return {
-    next() {
-      if (currentDepartment >= departments.length) {
-        return { done: true };
-      }
-
-      const department = departments[currentDepartment];
-      const employees = report.allEmployees[department];
-      if (currentEmployee >= employees.length) {
-        currentEmployee = 0;
-        currentDepartment++;
-      }
-
-      return { value: employees[currentEmployee++] };
-    },
-  };
+  const result = [];
+  for (const idx of Object.values(report.allEmployees)) {
+    result.push(...idx);
+  }
+  return result;
 }
