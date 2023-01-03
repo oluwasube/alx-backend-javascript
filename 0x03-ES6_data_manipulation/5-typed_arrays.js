e2c206ee6205adf458ed4a561a26fc73a548e6f5
oluwasube <enibeke20@gmail.com>
2023-01-03 22:58:31 +0000

    task 6 and 7

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position > length) {
-    throw new Error('Position outside range');
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
   }
-
-  const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  view[position] = value;
-  return buffer;
+  const newBuffer = new ArrayBuffer(length);
+  const int8 = new Int8Array(newBuffer, 0, length);
+  int8.set([value], position);
+  return new DataView(newBuffer);
 }
