deef00857f1c80a2b67772106d5d27624704436d
Oluwasube promise <enibeke20@gmail.com>
2023-02-24 23:51:55 +0100

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,10 +2,14 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('Hello Holberton School!');
   res.end();
 });
 
-app.listen(1245);
-
 module.exports = app;
+
+app.listen(1245, () => {
+  console.log('Server running on port 1245');
+});
+
