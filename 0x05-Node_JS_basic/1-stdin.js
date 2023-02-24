a167dec9b0e6f1c2849cef886b8016b25fabb3ba
oluwasube <enibeke20@gmail.com>
2023-02-24 13:43:24 +0100

    fixed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,6 @@
-/*
-function main() {
-  const readline = require('readline');
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-    rl.close();
-  });
-}
-
-if (require.main === module) {
-  main();
-}
-*/
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
-  process.stdout.write('Welcome to Holberton School, what is your name?\n');
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
