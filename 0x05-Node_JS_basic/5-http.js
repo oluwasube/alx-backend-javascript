d525b312492ac34670bf5a6347d77cb151fab368
oluwasube <enibeke20@gmail.com>
2023-02-25 12:47:08 +0100

    fixed

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -6,25 +6,19 @@ const app = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!\n');
   } else if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then(({ studentCount, students }) => {
-        const studentList = Object.entries(students)
-          .map(([field, list]) => `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`)
-          .join('\n');
-        const responseText = `This is the list of our students\nNumber of students: ${studentCount}\n${studentList}\n`;
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(responseText);
-      })
-      .catch((error) => {
-        console.error(error);
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal server error\n');
-      });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Page not found\n');
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      res.end(outString);
+    }).catch(() => {
+      res.statusCode = 404;
+      res.end('Cannot load the database');
+    });
   }
 });
 /* eslint-disable jest/require-hook */
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server running on port 1245');
+});
+
 module.exports = app;
